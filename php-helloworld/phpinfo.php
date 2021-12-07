dcef71871ef57fa703c66aa7a05e6cef7b0bd372
jcorrea21pr <jose.correa@claropr.com>
2021-12-07 15:53:07 -0500

    Adding phpinfo file

---
+++ php-helloworld/phpinfo.php
@@ -0,0 +1,8 @@
+<?php
+// Muestra toda la informaciópor defecto INFO_ALL
+phpinfo();
+
+// Muestra solamente la informacióe los móos.
+// // phpinfo(8) hace exactamente lo mismo.
+phpinfo(INFO_MODULES);
+?>
